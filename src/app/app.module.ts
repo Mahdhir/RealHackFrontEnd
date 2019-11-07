d956070bb0421dc055ffadc3c6dd18ec88b56a73
Mahdhi Rezvi <mahdhi_rezvi@yahoo.com>
2019-11-07 18:53:17 +0530

    Added Routing for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // required animations module
+    ToastrModule.forRoot() // ToastrModule added
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/services/auth-service.service';
+import { first } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  submitted = false;
+  value = 'password';
+  showPwd = 'octicon octicon-eye';
+  showPass = false;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService,
+    private toastCtrl: ToastrService
+    ) { }
+
+  ngOnInit() {
+    if (this.authService.isLoggedIn()) {
+      this.router.navigate(['/dashboard/info']);
+      return ;
+    }
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.required]
+  });
+  }
+
+  onSubmit() {
+    console.log('Submitted');
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      console.log('Invalid');
+      return;
+    }
+    // if (this.f.email.value === 'mahdhi@yahoo.com' && this.f.password.value === '1234' ) {
+    //     localStorage.setItem('token', '122345666');
+    //     this.router.navigate(['dashboard/info']);
+    // } else {
+    //   alert('Invalid Login');
+    // }
+    this.authService.login(this.f.email.value, this.f.password.value )
+    .pipe(first())
+    .subscribe(
+      data => {
+        console.log(data);
+        // tslint:disable-next-line:triple-equals
+        if (data.firstLogin == false) {
+          this.router.navigate(['dashboard/info']);
+        } else {
+          this.router.navigate(['dashboard/changePassword']);
+          this.toastCtrl.warning('Please change your first time login password', 'WARNING' );
+        }
+      },
+      error => {
+        console.log(error);
+        if (error.status === 400) {
+          console.log('Invalid Login');
+          this.toastCtrl.error('Invalid Login', 'Login Failed');
+        } else if (error.status === 401) {
+          this.toastCtrl.error('Please Check Your Email For Email Verification', 'Invalid Login');
+        } else {
+          console.log('Server Error');
+          this.toastCtrl.error('Server Error', 'Login Failed');
+        }
+      }
+    );
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  butClick() {
+    this.showPass ? this.showPwd = 'octicon octicon-eye-closed' : this.showPwd = 'octicon octicon-eye';
+  }
+}
