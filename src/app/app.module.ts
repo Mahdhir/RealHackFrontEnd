a28692e5c762cc8396556a24ac01f6134e6ca66b
Mahdhi Rezvi <mahdhi_rezvi@yahoo.com>
2019-11-07 19:28:44 +0530

    Login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AuthModule } from './auth/auth.module';
+import { AuthRoutingModule } from './auth/auth-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
-    ToastrModule.forRoot() // ToastrModule added
+    ToastrModule.forRoot(), // ToastrModule added
+    ReactiveFormsModule,
+    FormsModule,
+    AuthModule,
+    AuthRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
 import { AuthRoutingModule } from './auth-routing.module';
-import { ShowHidePasswordModule } from 'ngx-show-hide-password';
-import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
+// import { ShowHidePasswordModule } from 'ngx-show-hide-password';
+// import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
 
 
 
@@ -14,9 +14,10 @@ import { PasswordStrengthMeterModule } from 'angular-password-strength-meter';
   imports: [
     CommonModule,
     ReactiveFormsModule,
+    FormsModule,
     AuthRoutingModule,
-    ShowHidePasswordModule,
-    PasswordStrengthMeterModule
+    // ShowHidePasswordModule,
+    // PasswordStrengthMeterModule
   ]
 })
 export class AuthModule { }
