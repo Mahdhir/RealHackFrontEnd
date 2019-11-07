36d8f69cf0d915f2c38fb8f2e9015dde5303a887
Mahdhi Rezvi <mahdhi_rezvi@yahoo.com>
2019-11-07 19:47:03 +0530

    Sign up services and routing

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,20 +1,33 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-
+import {environment} from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
+  url = `${environment.url}api/v1/authentication`;
   constructor(private http:HttpClient) { }
 
   isLoggedIn(){
     return true;
   }
 
+  signUp(name,email,password){
+    let obj = {
+      name:name,
+      email:email,
+      password:password
+    };
+    return this.http.post<any>(`${this.url}/registerUser`,obj);
+  }
+
   login(email,password){
-    return this.http.get<any>('');
+    let obj = {
+      username:email,
+      password:password
+    };
+    return this.http.post<any>(`${this.url}/auth`,obj);
   }
 }
